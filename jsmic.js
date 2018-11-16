c42fc580140cca83e02b134f6f19819b7d868a41
Master <jeti2121@hotmail.com>
2018-11-16 23:31:34 +0100

    Update jsmic.js
    fixed indentation

--- jsmic.js
+++ jsmic.js
@@ -322,10 +322,10 @@
         view.setUint8(2, 'F'.charCodeAt(0));
         view.setUint8(3, 'F'.charCodeAt(0));
 
-                      view.setUint32(4, 44 + nbytes - 8, true); // file length
+        view.setUint32(4, 44 + nbytes - 8, true); // file length
 
-                      // RIFF type
-                      view.setUint8(8, 'W'.charCodeAt(0));
+        // RIFF type
+        view.setUint8(8, 'W'.charCodeAt(0));
         view.setUint8(9, 'A'.charCodeAt(0));
         view.setUint8(10, 'V'.charCodeAt(0));
         view.setUint8(11, 'E'.charCodeAt(0));
@@ -336,21 +336,21 @@
         view.setUint8(14, 't'.charCodeAt(0));
         view.setUint8(15, ' '.charCodeAt(0));
 
-                      view.setUint32(16, bps, true); // format chunk length
-                      view.setUint16(20, 1, true); // sample format (raw)
-                      view.setUint16(22, nch, true); // channel count
-                      view.setUint32(24, samplerate, true); // sample-rate
-                      view.setUint32(28, samplerate * nch * bps / 8, true); // byte-rate (= samplerate * nchannels * bitspersample / 8bitsin1byte)
-                      view.setUint16(32, nch * bps / 8, true); // block align (= nchannels * bytespersample / 8bitsin1byte)
-                      view.setUint16(34, bps, true); // bits per sample
+        view.setUint32(16, bps, true); // format chunk length
+        view.setUint16(20, 1, true); // sample format (raw)
+        view.setUint16(22, nch, true); // channel count
+        view.setUint32(24, samplerate, true); // sample-rate
+        view.setUint32(28, samplerate * nch * bps / 8, true); // byte-rate (= samplerate * nchannels * bitspersample / 8bitsin1byte)
+        view.setUint16(32, nch * bps / 8, true); // block align (= nchannels * bytespersample / 8bitsin1byte)
+        view.setUint16(34, bps, true); // bits per sample
 
-                      // data chunk identifier
-                      view.setUint8(36, 'd'.charCodeAt(0));
-                      view.setUint8(37, 'a'.charCodeAt(0));
-                      view.setUint8(38, 't'.charCodeAt(0));
-                      view.setUint8(39, 'a'.charCodeAt(0));
+        // data chunk identifier
+        view.setUint8(36, 'd'.charCodeAt(0));
+        view.setUint8(37, 'a'.charCodeAt(0));
+        view.setUint8(38, 't'.charCodeAt(0));
+        view.setUint8(39, 'a'.charCodeAt(0));
 
-                      view.setUint32(40, nbytes, true); // data chunk length
+        view.setUint32(40, nbytes, true); // data chunk length
       }
 
       // view.setInt16, 3rd param is true for little endian (LE), audio-format: S16_LE
